a7f461ed4eb7bfc3bf8c66d0c72e1bd0121ebbee
markvong-o <mark.vong@okta.com>
2026-08-11 13:00:45 -0700

    Remove orphaned CSS files and bespoke Badge component missed by earlier cleanup

--- client/src/components/Badge.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Badge.css';
-
-export function Badge({ children, variant = 'default' }) {
-  return <span className={`badge badge-${variant}`}>{children}</span>;
-}
